9c4d788b9d531d0bf7fe8bf4d0f3a61178fd90f9
muntashir-wahid <muntashir.wahid@yahoo.com>
2023-06-04 20:32:19 +0600

    created separate routers for separate recources and mounted that on a route

--- index.js
+++ index.js
@@ -127,16 +127,14 @@ app.listen(PORT, () => {
 
 // Routes
 
-app.route("/api/v1/tours").get(getAllTours).post(createTour);
-app
-  .route("/api/v1/tours/:id")
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app.route("/api/v1/users").get(getUsers).post(createUser);
-app
-  .route("/api/v1/users/:id")
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+tourRouter.route("/").get(getAllTours).post(createTour);
+tourRouter.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
+
+userRouter.route("/").get(getUsers).post(createUser);
+userRouter.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
+
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
