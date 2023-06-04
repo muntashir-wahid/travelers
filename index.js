5e88d6ed8653e112b9b3cfac02795f754d17d6f3
muntashir-wahid <muntashir.wahid@yahoo.com>
2023-06-04 15:54:13 +0600

    wrote boilerplate code for tours CRUD

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const PORT = 8000;
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res
@@ -10,6 +11,62 @@ app.get("/", (req, res) => {
     .json({ message: "Hello form the server side!", app: "Natours" });
 });
 
+app.get("/api/v1/tours", (req, res) => {
+  console.log("Get all Tours");
+  res.status(200).json({
+    status: "success",
+    data: {
+      message: "Waiting for the database connection",
+    },
+  });
+});
+
+app.get("/api/v1/tours/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(`Get tour: ID:${id}`);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      message: "Waiting for the database connection",
+    },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const tour = req.body;
+  console.log("Create tour:", tour);
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      message: "Waiting for the database connection",
+    },
+  });
+});
+
+app.patch("/api/v1/tours/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(`Update  tour: ID=${id}`);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      message: "Waiting for the database connection",
+    },
+  });
+});
+
+app.delete("/api/v1/tours/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(`Delete tour: ID=${id}`);
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
+
 app.listen(PORT, () => {
-  console.log(`Natours app is running on prot ${PORT}`);
+  console.log(`Travelers app is running on prot ${PORT}`);
 });
