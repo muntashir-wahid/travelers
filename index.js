62fbb1600906b87d0fa451fedcd5886c9deb5cc6
muntashir-wahid <muntashir.wahid@yahoo.com>
2023-06-18 21:07:23 +0600

    installed mongoose and connected the database

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+const mongoose = require("mongoose");
 require("dotenv").config();
-
 const app = require("./app");
 
+// Create MongoDb Connection string
+const connectionString = process.env.DB_CONNECTION_STRING.replace(
+  "<PASSWORD>",
+  process.env.DB_PASSWORD
+);
+
 const PORT = process.env.PORT || 5000;
+
+// Connect to the Database
+
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database successfully connected."))
+  .catch((err) => console.log(err));
 
 // Start the server
 app.listen(PORT, () => {
