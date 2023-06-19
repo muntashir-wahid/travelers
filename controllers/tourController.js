f98d93d92cd3e2cd466158bd2989b4d0a3b7af91
muntashir-wahid <muntashir.wahid@yahoo.com>
2023-06-19 08:05:35 +0600

    made the controller functions to create and find documents from the db

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,36 +1,58 @@
 const Tour = require("./../models/tourModel");
 
-exports.getAllTours = (req, res) => {
-  console.log("Get all Tours");
-  res.status(200).json({
-    status: "success",
-    data: {
-      message: "Waiting for the database connection",
-    },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: "success",
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 400,
+      message: "Something went wrong.",
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  const { id } = req.params;
-  console.log(`Get tour: ID:${id}`);
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
 
-  res.status(200).json({
-    status: "success",
-    data: {
-      message: "Waiting for the database connection",
-    },
-  });
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 400,
+      message: "Something went wrong.",
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
-  const tour = await Tour.create(req.body);
+  try {
+    const newTour = await Tour.create(req.body);
 
-  res.status(201).json({
-    status: "success",
-    data: {
-      tour,
-    },
-  });
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: "Invalid data sent.",
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
